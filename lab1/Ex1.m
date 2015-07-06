0e9202610ed7e3c5c51c7fb2c2a0754cea3dbad7
ios4 <ios4@serbans-mbp-2.ad.qualitance.com>
2015-07-06 15:21:17 +0300

    final ex 1

--- lab1/Ex1.m
+++ lab1/Ex1.m
@@ -37,19 +37,18 @@
     NSMutableString *newString = [my_string substringToIndex:([my_string length] - 1)];
     
     NSLog(@"%@", newString);
-    return my_string;
+    return newString;
 }
 
 -(NSString *)linkAndCapitalizeAllWordsFromArray:(NSArray *)words usingSeparator:(NSString *)separator
 {
-    Ex1 *ex1 = [[Ex1 alloc] init];
+ //   Ex1 *ex1 = [[Ex1 alloc] init];
     
     NSArray *my_first_string = [[NSArray alloc] init];
-    NSString *my_second_string = [[NSString alloc] init];
     NSMutableString *my_final_string = [[NSMutableString alloc] init];
     
-    my_first_string = [self cappitalizeWordsFromArray];
-    
+    my_first_string = [self cappitalizeWordsFromArray:words];
+    my_final_string = [self linkAllWordsFromArray:my_first_string usingSeparator:separator];
     
     NSLog(@"%@", my_final_string);
     return my_final_string;
@@ -57,19 +56,62 @@
 
 -(NSString *)extractSubstringFromString:(NSString *)string starting:(NSUInteger)startIndex ending:(NSUInteger)endIndex
 {
-    //ex1
-    return nil;
+    NSString *my_string = [[NSMutableString alloc] init];
+    
+    my_string = [string substringWithRange:NSMakeRange(startIndex, endIndex - startIndex)];
+    
+    NSLog(@"%@", my_string);
+    return my_string;
 }
 
 -(BOOL)checkPalindrome:(NSString *)string
 {
-    //ex1
+    NSMutableString *reversedString = [[NSMutableString alloc] init];
+    NSInteger charIndex = [string length];
+    
+    while (charIndex > 0) {
+        charIndex--;
+        NSRange subStrRange = NSMakeRange(charIndex, 1);
+        [reversedString appendString:[string substringWithRange:subStrRange]];
+    }
+    
+    NSLog(@"%@", reversedString);
+    
+    if([string isEqualToString:reversedString])
+    {
+        NSLog(@"YES");
+        return YES;
+    }
     return NO;
 }
 
 -(BOOL)checkPalindromeFromCharPosition:(NSUInteger)index ofAllWordsFromArray:(NSArray *)array
 {
-    //ex1
+    NSMutableString *string = [[NSMutableString alloc] init];
+
+    char c;
+    
+    for(NSString *elem in array)
+    {
+        if(index < [elem length])
+        {
+            c = [elem characterAtIndex:index];
+            NSString *s = [NSString stringWithFormat:@"%c", c];
+        
+            NSLog(@"%@", s);
+        
+            [string appendString:s];
+        }
+        else
+            return NO;
+    }
+    
+    NSLog(@"%@", string);
+    
+    if([self checkPalindrome:string])
+        return YES;
+    
     return NO;
 }
+
 @end
